a39bac03b811e96074ea4e1d222602d727c16390
Harry Sadoyan <harrysadoyan@gmail.com>
2018-04-11 11:29:54 -0400

    Truncated SVD initial commit

--- movies.py
+++ movies.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.stats import randint as sp_randint
 from sklearn.datasets import load_files
 from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
 
 # Models to try
 from sklearn.ensemble import RandomForestClassifier
@@ -32,66 +34,55 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 count_vect = CountVectorizer()
-X_train_count = count_vect.fit_transform(X_train)
-
 tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_count)
+pca = TruncatedSVD()
 
-pca = PCA(n_components=20)
-X_train_array = X_train_tfidf.toarray()
-pca.fit(X_train_array)
-
-X_train_pca = pca.transform(X_train_array)
-
+pipe = Pipeline([
+    ('vect', count_vect),
+    ('tfidf', tfidf_transformer),
+    ('pca', pca),
+    ('clf', RandomForestClassifier())
+])
 
 # Plot the Principal Components
-plt.figure()
-plt.plot(pca.explained_variance_)
-plt.xlabel('number of components')
-plt.ylabel('individual explained variance')
-plt.show()
+#  plt.figure()
+#  plt.plot(pca.explained_variance_)
+#  plt.xlabel('number of components')
+#  plt.ylabel('individual explained variance')
+#  plt.show()
 
-# Test data transformations
-X_test_count = count_vect.transform(X_test)
-X_tfidf_test = tfidf_transformer.transform(X_test_count)
-X_test_pca = pca.transform(X_tfidf_test.toarray())
-
-clf = RandomForestClassifier()
 
 # specify parameters and distributions to sample from
 parameters_rand = {
-    "n_estimators": sp_randint(300, 2000),
-    "max_depth": [3, None],
-    "max_features": sp_randint(1, 11),
-    "min_samples_split": sp_randint(2, 11),
-    "min_samples_leaf": sp_randint(1, 11),
-    "bootstrap": [True, False],
-    "criterion": ["gini", "entropy"]
+    "pca__n_components": sp_randint(10, 50),
+    "clf__n_estimators": sp_randint(300, 2000),
+    "clf__min_samples_split": sp_randint(2, 20),
+    "clf__min_samples_leaf": sp_randint(2, 20),
+    "clf__bootstrap": [True, False],
+    "clf__criterion": ["gini", "entropy"]
 }
 
 # run randomized search
 # Accuracy should be comparable to grid search, but runs much much faster
-n_iter_search = 40
-random_search = RandomizedSearchCV(clf, param_distributions=parameters_rand,
+n_iter_search = 20
+random_search = RandomizedSearchCV(pipe, param_distributions=parameters_rand,
                                    n_iter=n_iter_search,
                                    n_jobs=-1)
 
-random_search.fit(X_train_pca, y_train)
+random_search.fit(X_train, y_train)
+predicted = random_search.predict(X_test)
 
-predicted = random_search.predict(X_test_pca)
-
-np.mean(predicted == y_test)
-
+print(np.mean(predicted == y_test))
 
 #  # use a full grid over all parameters
 #  param_grid_grid = {
-    #  "n_estimators": [30, 50],
-    #  "max_depth": [3, None],
-    #  "max_features": [1, 3, 10],
-    #  "min_samples_split": [2, 3, 10],
-    #  "min_samples_leaf": [1, 3, 10],
-    #  "bootstrap": [True, False],
-    #  "criterion": ["gini", "entropy"]
+#  "n_estimators": [30, 50],
+#  "max_depth": [3, None],
+#  "max_features": [1, 3, 10],
+#  "min_samples_split": [2, 3, 10],
+#  "min_samples_leaf": [1, 3, 10],
+#  "bootstrap": [True, False],
+#  "criterion": ["gini", "entropy"]
 #  }
 
 #  # run grid search
