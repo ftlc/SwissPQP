a25ecaff9d90fdde5b15e1d42e8dd86834099c97
grbishop <grbishop@wpi.edu>
2018-04-17 05:41:22 -0400

    randomize domized search for parameters

--- resumes.py
+++ resumes.py
@@ -11,6 +11,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+
+from scipy.stats import randint as sp_randint
 from sklearn.model_selection import RandomizedSearchCV
 
 resumedir = 'data/resumes_and_reviews'
@@ -26,12 +28,42 @@ X_train, X_test, y_train, y_test = train_test_split(
     x_work, y_work, test_size=0.10
 )
 
-text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
-                     ('svd', TruncatedSVD(n_components=20)),
-                     ('clf', RandomForestClassifier()),
-])
+count_vect = CountVectorizer()
+X_train_count = count_vect.fit_transform(X_train)
 
-text_clf.fit(X_train, y_train)
-predicted = text_clf.predict(X_test)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_count)
+
+svd = TruncatedSVD(n_components=20)
+svd.fit(X_train_tfidf)
+print("svd explained variance: {}".format(svd.explained_variance_.sum()))
+x_train_svd = svd.transform(X_train_tfidf)
+
+# Test data transformations
+X_test_count = count_vect.transform(X_test)
+X_tfidf_test = tfidf_transformer.transform(X_test_count)
+x_test_svd = svd.transform(X_tfidf_test)
+
+clf = RandomForestClassifier()
+
+parameters_rand = {
+    "n_estimators": sp_randint(300, 2000),
+    "max_depth": [3, None],
+    "max_features": sp_randint(1, 11),
+    "min_samples_split": sp_randint(2, 11),
+    "min_samples_leaf": sp_randint(1, 11),
+    "bootstrap": [True, False],
+    "criterion": ["gini", "entropy"]
+}
+
+n_iter_search = 40
+random_search = RandomizedSearchCV(clf, param_distributions=parameters_rand,
+                                   n_iter=n_iter_search,
+                                   n_jobs=-1)
+
+random_search.fit(x_train_svd, y_train)
+predicted = random_search.predict(x_test_svd)
+print("Parameters: {}".format(random_search.best_params_))
+
+
 print("Classifier accuracy: {}".format(np.mean(predicted == y_test)))
