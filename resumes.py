ff1f5851cd3b975c50277cbcb5296dee24564dfd
Harry Sadoyan <harrysadoyan@gmail.com>
2018-04-25 16:47:59 -0400

    Add f1 score and confusion matrix

--- resumes.py
+++ resumes.py
@@ -1,4 +1,5 @@
-# Trying to train a model to distinguish movie industry resumes from movie reviews
+# Trying to train a model to distinguish movie industry resumes from 
+# movie reviews
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,10 +11,8 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-
-from scipy.stats import randint as sp_randint
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 
 resumedir = 'data/resumes_and_reviews'
 resumes = load_files(resumedir, shuffle=True)
@@ -33,7 +32,7 @@ top_words = []
 
 svd = TruncatedSVD(n_components=20)
 svd.fit(X_train_tfidf)
-#print("svd explained variance: {}".format(svd.explained_variance_.sum()))
+# print("svd explained variance: {}".format(svd.explained_variance_.sum()))
 X_train_svd = svd.transform(X_train_tfidf)
 
 # Test data transformations
@@ -41,13 +40,15 @@ X_test_count = count_vect.transform(X_test)
 X_tfidf_test = tfidf_transformer.transform(X_test_count)
 X_test_svd = svd.transform(X_tfidf_test)
 
-params = {'bootstrap': False,
+params = {
+    'bootstrap': False,
     'criterion': 'entropy',
     'max_depth': 3,
     'max_features': 5,
     'min_samples_leaf': 1,
     'min_samples_split': 2,
-    'n_estimators': 457}
+    'n_estimators': 457
+}
 
 
 clf = RandomForestClassifier(**params)
@@ -55,3 +56,12 @@ clf.fit(X_train_svd, y_train)
 predicted = clf.predict(X_test_svd)
 
 print("Classifier accuracy: {}".format(np.mean(predicted == y_test)))
+print("Score: {}".format(clf.score(X_test_svd, y_test)))
+
+confusion_matrix(y_test, predicted)
+tn, fp, fn, tp = confusion_matrix(y_test, predicted).ravel()
+f1 = f1_score(y_test, predicted)
+
+print("Confusion Matrix: \n{}".format(confusion_matrix(y_test, predicted)))
+print("F1 Score: {}".format(f1))
+
