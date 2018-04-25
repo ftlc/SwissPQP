3c2c4d551139595b678eb048bc89b86002e4af04
leogrande <leosgrande1@gmail.com>
2018-04-24 22:53:55 -0400

    Added a news file to resume_reviews. Changed dir path in movies.py. Need to just figure out the bytes error in alt.atheism in data

--- movies.py
+++ movies.py
@@ -16,10 +16,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 
+
+from keras.datasets import mnist
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input
+from keras.optimizers import Adam
+
+
 # Import Data
 # Downloaded from http://www.nltk.org/nltk_data/
-moviedir = 'data/movie_reviews/txt_sentoken/'
-reviews = load_files(moviedir, shuffle=True)
+# moviedir = 'data/movie_reviews/txt_sentoken/'
+moviedir = 'data/resumes_and_reviews'
+reviews = load_files(moviedir, shuffle=True, encoding='utf-8')
 
 # Hide some data in the vault to avoid snooping
 x_work, x_vault, y_work, y_vault = train_test_split(
@@ -37,7 +45,7 @@ X_train_count = count_vect.fit_transform(X_train)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_count)
 
-pca = PCA(n_components=20)
+pca = PCA(n_components=120)
 X_train_array = X_train_tfidf.toarray()
 pca.fit(X_train_array)
 
@@ -46,15 +54,15 @@ X_train_pca = pca.transform(X_train_array)
 
 # Plot the Principal Components
 plt.figure()
-plt.plot(pca.explained_variance_.cumsum())
+plt.plot(pca.explained_variance_)
 plt.xlabel('number of components')
 plt.ylabel('cumulative explained variance')
 plt.show()
 
 # Test data transformations
 X_test_count = count_vect.transform(X_test)
-X_tfidf_test = tfidf_transformer.transform(X_test_count)
-X_test_pca = pca.transform(X_tfidf_test.toarray())
+X_test_tfidf = tfidf_transformer.transform(X_test_count)
+X_test_pca = pca.transform(X_test_tfidf.toarray())
 
 clf = RandomForestClassifier()
 
@@ -80,23 +88,46 @@ random_search.fit(X_train_pca, y_train)
 
 predicted = random_search.predict(X_test_pca)
 
-print(np.mean(predicted == y_test))
+print("PCA with random forest")
+print("Accuracy" + int(np.mean(predicted == y_test)))
+
+# AutoEncoder
+
+input_shape = X_train_tfidf.shape[1]
+
+ae = Sequential()
+ae.add(Dense(512,  activation='elu', input_shape=(input_shape,)))
+ae.add(Dense(128,  activation='elu'))
+ae.add(Dense(1,    activation='linear', name="bottleneck"))
+ae.add(Dense(128,  activation='elu'))
+ae.add(Dense(512,  activation='elu'))
+ae.add(Dense(input_shape,  activation='sigmoid', name="out_layer"))
+ae.compile(loss='binary_crossentropy',
+           optimizer='adadelta',
+           metrics=['accuracy'])
 
 
-#  # use a full grid over all parameters
-#  param_grid_grid = {
-    #  "n_estimators": [30, 50],
-    #  "max_depth": [3, None],
-    #  "max_features": [1, 3, 10],
-    #  "min_samples_split": [2, 3, 10],
-    #  "min_samples_leaf": [1, 3, 10],
-    #  "bootstrap": [True, False],
-    #  "criterion": ["gini", "entropy"]
-#  }
+history = ae.fit(X_train_tfidf, X_train_tfidf,
+                 batch_size=128,
+                 epochs=5,
+                 verbose=1,
+                 validation_data=(X_test_tfidf, X_test_tfidf))
 
-#  # run grid search
-#  grid_search = GridSearchCV(clf, param_grid=param_grid_grid, n_jobs=-1)
-#  grid_search.fit(X_train_pca, y_train)
 
-#  predicted_grid = grid_search.predict(X_test_pca)
-#  np.mean(predicted == y_test)
+encoder = Model(ae.input, ae.get_layer('bottleneck').output)
+
+encoder.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
+
+encoder.fit(X_train_tfidf, y_train,
+            nb_epoch=10,
+            batch_size=128,
+            shuffle=True,
+            validation_data=(X_test_tfidf, y_test))
+
+print("Autoencoder")
+scores = encoder.evaluate(X_test_tfidf, y_test, verbose=1)
+print("Accuracy: ", scores[1])
+
